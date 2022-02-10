1fa38beadd55b87102ea77305c887360e98383cd
ivc17044adm <ivc17044adm@IVC17-044.lan>
2022-02-09 21:26:57 -0600

    First Commit to local

---
+++ FirstProjectToGithub/src/com/first/git/testPackage/FirstGitTestClass.java
@@ -0,0 +1,11 @@
+package com.first.git.testPackage;
+
+public class FirstGitTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My First Github project -> First Class");
+
+	}
+
+}
