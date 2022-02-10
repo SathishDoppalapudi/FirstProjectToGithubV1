f236d138b9d454baa24a5346de35e21987d28803
John Doe <john.doe@example.com>
2022-02-09 22:59:23 -0600

    commit

---
+++ FirstProjectToGithub/src/com/first/git/testPackage/Test2.java
@@ -0,0 +1,11 @@
+package com.first.git.testPackage;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is my Second Test");
+	}
+
+}
